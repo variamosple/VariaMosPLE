91872e70c08d4c1fffd46a8b3a6e943d01576fe8
Mauro Agudelo <mauro.0116@gmail.com>
2021-04-22 09:09:13 -0500

    VariaMosPLE Microservices  0.3

--- src/DataProvider/Services/languageService.tsx
+++ src/DataProvider/Services/languageService.tsx
@@ -1,15 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const apiVariamos = axios.create({
-    baseURL: 'http://localhost:4000/'
-}) 
+  baseURL: "http://localhost:4000/",
+});
 
-export function getLanguages() {
-    try {
-        apiVariamos.get('/languages').then(res => {
-            console.log(res.data)
-        })
-    } catch (error) {
-        console.log("Test Wrong")
-    }
+export function getLanguages(callBack: any) {
+  try {
+    apiVariamos.get("/languages").then((res) => {
+      console.log(res.data);
+      callBack(res.data);
+    });
+  } catch (error) {
+    console.log("Test Wrong");
+  }
 }
+
+
+export function getLanguagesNative(
+  method: string,
+  url: string,
+  languageName: string
+) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr);
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.send(languageName);
+  });
+}
+
--- src/Infraestructure/project/ProjectService.ts
+++ src/Infraestructure/project/ProjectService.ts
@@ -1,37 +1,23 @@
+import { getLanguages, getLanguagesNative } from "../../DataProvider/Services/languageService";
+
 export default class ProjectService {
-    private graph: any;
-    private project: any;
+  private graph: any;
+  private project: any;
 
-    constructor() {
-        //instancia project
-    }
+  constructor() {
+    //instancia project
+  }
 
-    setGraph(graph: any) {
-        this.graph = graph;
-    }
-    getGraph() {
-        return this.graph;
-    }
-    open() {
-        //open file
-    }
-    getStyleDefinition(language: String) {
-        let str = '{' +
-        '"elements": { '+
-        '"RootFeature": {'+
-        '"design": "shape=rectangle;",'+
-        '"width": 100,'+
-        '"height": 50'+
-        '},'+
-        '"Bundle": {'+
-        '"design": "shape=ellipse;perimter=ellipsePerimeter",'+
-        '"width": 100,'+
-        '"height": 50'+
-        '}'+
-        '}'+
-        '}';
-
-
-        return JSON.parse(str);
-    }
+  setGraph(graph: any) {
+    this.graph = graph;
+  }
+  getGraph() {
+    return this.graph;
+  }
+  open() {
+    //open file
+  }
+  getStyleDefinition(language: string, callBack:any) {
+    getLanguages(callBack);
+  }
 }
