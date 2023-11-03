e2b653da6e6cafe8996f5e8804a3e4b97324c486
ccr185 <camilo.correa-restrepo@univ-paris1.fr>
2023-11-03 16:56:51 +0100

    bug fixes and modal improvements

--- src/Domain/ProductLineEngineering/UseCases/QueryUseCases.ts
+++ src/Domain/ProductLineEngineering/UseCases/QueryUseCases.ts
@@ -1,38 +1,92 @@
-
 import axios from "axios";
 import ProjectService from "../../../Application/Project/ProjectService";
 import * as alertify from "alertifyjs";
 import { Query } from "../Entities/Query";
+import { slugify } from "../../../Addons/Library/Utils/Utils";
+import ProjectUseCases from "./ProjectUseCases";
+import { Element } from "../Entities/Element";
+import { Property } from "../Entities/Property";
 
-export function syncSemantics(projectService: ProjectService, translatorEndpoint: string){
-    return runQuery(projectService, translatorEndpoint, new Query(JSON.parse('{"solver": "minizinc","operation": "get_model"}')))
+export function syncSemantics(
+  projectService: ProjectService,
+  translatorEndpoint: string
+) {
+  return runQuery(
+    projectService,
+    translatorEndpoint,
+    new Query(JSON.parse('{"solver": "minizinc","operation": "get_model"}'))
+  );
+}
+
+export function syncConcreteSemantics(
+  projectService: ProjectService,
+  translatorEndpoint: string,
+  lang: string
+) {
+  return runQuery(
+    projectService,
+    translatorEndpoint,
+    new Query(JSON.parse(`{"solver": "${lang}","operation": "get_code"}`))
+  );
 }
 
 export function runQuery(
-    projectService: ProjectService, translatorEndpoint: string, query: Query
+  projectService: ProjectService,
+  translatorEndpoint: string,
+  query: Query
 ) {
-    // We must build the request body both from the query and the project
-    // information.
+  // We must build the request body both from the query and the project
+  // information.
 
-    // First, get the project information.
-    // get currently selected language
-    const semantics = projectService.currentLanguage.semantics;
-    const data = {
-        rules: semantics,
-        query: query,
-        modelSelectedId: projectService.getTreeIdItemSelected(),
-        transactionId: projectService.generateId(),
-        project: projectService.project
+  // First, get the project information.
+  // get currently selected language
+  const semantics = projectService.currentLanguage.semantics;
+  const data = {
+    rules: semantics,
+    query: query,
+    modelSelectedId: projectService.getTreeIdItemSelected(),
+    transactionId: projectService.generateId(),
+    project: projectService.project,
+  };
+  alertify.success("request sent ...");
+  return axios
+    .post(translatorEndpoint, { data })
+    .then((response) => {
+      alertify.success("request successful ...");
+      return response.data.data.content;
+    })
+    .catch((error) => {
+      alertify.error("something went wrong ...");
+      console.error(error);
+      return null;
+    });
+}
+
+//This function sanitizes the concrete semantics by replacing the id
+//of the elements with a sluggified version of their name for readability
+export function sanitizeConcreteSemantics(
+  concreteSemantics: string,
+  projectService: ProjectService
+) {
+  //Regex that matches the pattern UUID_ followed by a uuid version 4
+  //with the hyphens replaced with underscores
+  //Example: UUID_12345678_1234_1234_1234_123456789abc
+  const uuidRegex =
+    /UUID_[a-f0-9]{8}_[a-f0-9]{4}_[a-f0-9]{4}_[a-f0-9]{4}_[a-f0-9]{12}/g;
+  //for each match, replace it with a sluggified version of the name
+  return concreteSemantics.replace(uuidRegex, (match) => {
+    const id = match.substring(match.indexOf("_") + 1).replaceAll("_", "-");
+    console.log(id);
+    //find the element in the model
+    const element: Element = projectService.findModelElementByIdInProject(id);
+    //if the element doesn't exist, check if it exists as a property
+    if (!element) {
+      const [propElem, property] = projectService.findModelElementPropertyByIdInProject(id);
+      if (propElem && property) {
+        return `${slugify((propElem as Element).name)}_${slugify((property as Property).name)}`;
+      }
+    } else {
+      return slugify(element.name);
     }
-    alertify.success('request sent ...');
-    return axios.post(translatorEndpoint, {data})
-        .then(response => {
-            alertify.success('request successful ...');
-            return response.data.data.content;
-        })
-        .catch(error => {
-            alertify.error('something went wrong ...');
-            console.error(error);
-            return null;
-        });
+  });
 }
--- src/UI/Queries/queryResult.tsx
+++ src/UI/Queries/queryResult.tsx
@@ -14,27 +14,44 @@ type QueryResultProps = {
   index: number;
   result: object | Array<any> | boolean;
   projectService: ProjectService;
-
-}
+};
 
 function isIterationResult(result: Array<any> | object | boolean): boolean {
-  return Array.isArray(result) && result.some((elem) => Array.isArray(elem) && elem.length === 2);
+  return (
+    Array.isArray(result) &&
+    result.some((elem) => Array.isArray(elem) && elem.length === 2)
+  );
 }
 
-export default function QueryResult(
-  { index, result, projectService }: QueryResultProps
-) {
+function hasFailedIterationResult(
+  result: Array<any> | object | boolean
+): boolean {
+  return (
+    Array.isArray(result) &&
+    result.some((elem) => Array.isArray(elem) && elem.length === 2 && !elem[1])
+  );
+}
 
+export default function QueryResult({
+  index,
+  result,
+  projectService,
+}: QueryResultProps) {
   const [paginationSelection, setPaginationSelection] = useState(0);
 
   const visibleResults = useMemo(() => {
     if (Array.isArray(result) && isIterationResult(result)) {
       // We know then that these are iteration results
       // We will now strip the first part of the strings in the first element
-      return result
-        .map((elem) => [elem[0].replace("UUID_", "").replaceAll("_","-"), elem[1]])
-        // Let's filter only those that are false
-        .filter((elem) => !elem[1]);
+      return (
+        result
+          .map((elem) => [
+            elem[0].replace("UUID_", "").replaceAll("_", "-"),
+            elem[1],
+          ])
+          // Let's filter only those that are false
+          .filter((elem) => !elem[1])
+      );
     } else {
       return result;
     }
@@ -44,69 +61,85 @@ export default function QueryResult(
     //we'll use the ids in the first element of the array to create overlays
     //for the elements in the graph.
     const graph = projectService.getGraph();
-    (visibleResults as Array<Array<string | boolean>>).forEach(([elem_id, _]) => {
-
-      // const cell = MxgraphUtils.findVerticeById(graph, elem_id, null);
-      const cell = graph.getModel().filterDescendants((cell) => {
-        if (cell.value) {
-          const uid = cell.value.getAttribute("uid");
-          if (uid === elem_id) {
-            return true;
+    (visibleResults as Array<Array<string | boolean>>).forEach(
+      ([elem_id, _]) => {
+        // const cell = MxgraphUtils.findVerticeById(graph, elem_id, null);
+        const cell = graph.getModel().filterDescendants((cell) => {
+          if (cell.value) {
+            const uid = cell.value.getAttribute("uid");
+            if (uid === elem_id) {
+              return true;
+            }
           }
+          return false;
+        })[0];
+
+        //const cell = graph.getModel().getCell(elem_id);
+        if (cell) {
+          const overlayFrame = new mx.mxCellOverlay(
+            new mx.mxImage(
+              "images/models/Eo_circle_red_white_no-entry.svg",
+              24,
+              24
+            ),
+            "Failed Query"
+          );
+          overlayFrame.align = mx.mxConstants.ALIGN_LEFT;
+          overlayFrame.verticalAlign = mx.mxConstants.ALIGN_TOP;
+          overlayFrame.offset = new mx.mxPoint(0, 0);
+
+          overlayFrame.addListener(mx.mxEvent.CLICK, function (_sender, _evt) {
+            graph.removeCellOverlay(cell, overlayFrame);
+          });
+
+          graph.addCellOverlay(cell, overlayFrame);
+          graph.refresh();
         }
-        return false;
-      })[0];
-
-      //const cell = graph.getModel().getCell(elem_id);
-      if (cell) {
-        const overlayFrame = new mx.mxCellOverlay(new mx.mxImage("images/models/Eo_circle_red_white_no-entry.svg", 24, 24), 'Failed Query');
-        overlayFrame.align = mx.mxConstants.ALIGN_LEFT;
-        overlayFrame.verticalAlign = mx.mxConstants.ALIGN_TOP;
-        overlayFrame.offset = new mx.mxPoint(0, 0);
-
-        overlayFrame.addListener(mx.mxEvent.CLICK, function (_sender, _evt) {
-          graph.removeCellOverlay(cell, overlayFrame);
-        });
-
-        graph.addCellOverlay(cell, overlayFrame);
-        graph.refresh();
       }
-    });
-  }
+    );
+  };
 
   const handleVisualize = () => {
     //Save the current project state so that we can restore it later
     console.info("Visualizing solution ", index, " of query result", result);
     if (!Array.isArray(result)) {
-      projectService.updateProject(
-        result as Project, projectService.getTreeIdItemSelected()
+      projectService.updateSelection(
+        result as Project,
+        projectService.getTreeIdItemSelected()
       );
-      projectService.lookupAndReselectModel();
+      // projectService.updateProject(
+      //   result as Project, projectService.getTreeIdItemSelected()
+      // );
+      // projectService.lookupAndReselectModel();
       projectService.getGraph().refresh();
     } else {
       if (isIterationResult(result)) {
         handleIterationQueryVisualization();
       } else {
-        projectService.updateProject(
-          result[paginationSelection] as Project, projectService.getTreeIdItemSelected()
+        projectService.updateSelection(
+          result[paginationSelection] as Project,
+          projectService.getTreeIdItemSelected()
         );
-        projectService.lookupAndReselectModel();
+        // projectService.updateProject(
+        //   result as Project, projectService.getTreeIdItemSelected()
+        // );
+        // projectService.lookupAndReselectModel();
         projectService.getGraph().refresh();
       }
     }
-  }
+  };
 
   return (
     <>
       <ListGroup horizontal className="flex d-flex my-2">
-        <ListGroup.Item className="flex-fill d-flex align-items-center justify-content-center">Solution {index}</ListGroup.Item>
-        {Array.isArray(visibleResults) &&
-          <ListGroup.Item
-            className="flex-fill d-flex align-items-center justify-content-center"
-          >
-            {visibleResults.length > 0 ?
+        <ListGroup.Item className="flex-fill d-flex align-items-center justify-content-center">
+          Solution {index}
+        </ListGroup.Item>
+        {Array.isArray(visibleResults) && (
+          <ListGroup.Item className="flex-fill d-flex align-items-center justify-content-center">
+            {visibleResults.length > 0 && !isIterationResult(visibleResults) ? (
               <Pagination>
-                {visibleResults.map((_, idx) =>
+                {visibleResults.map((_, idx) => (
                   <Pagination.Item
                     key={idx}
                     active={idx === paginationSelection}
@@ -114,26 +147,31 @@ export default function QueryResult(
                   >
                     {idx + 1}
                   </Pagination.Item>
-                )}
+                ))}
               </Pagination>
-              : "No element failed query"
-            }
+            ) : hasFailedIterationResult(visibleResults) ? (
+              `${visibleResults.length} elements failed query`
+            ) : (
+              "No element failed query"
+            )}
           </ListGroup.Item>
-        }
-        {typeof result === "object" ?
-          <ListGroup.Item
-            className="flex-fill d-flex align-items-center justify-content-center"
-          >
-            <Button size="sm" onClick={handleVisualize}>Visualize</Button>
-          </ListGroup.Item> :
-          typeof result === "boolean" &&
-          <ListGroup.Item
-            className="flex-fill d-flex align-items-center justify-content-center"
-          >
-            {result ? "SAT" : "UNSAT"}
+        )}
+        {(isIterationResult(result) &&
+          hasFailedIterationResult(result as Array<any>)) ||
+        (typeof result === "object" && !isIterationResult(result)) ? (
+          <ListGroup.Item className="flex-fill d-flex align-items-center justify-content-center">
+            <Button size="sm" onClick={handleVisualize}>
+              Visualize
+            </Button>
           </ListGroup.Item>
-        }
+        ) : (
+          typeof result === "boolean" && (
+            <ListGroup.Item className="flex-fill d-flex align-items-center justify-content-center">
+              {result ? "SAT" : "UNSAT"}
+            </ListGroup.Item>
+          )
+        )}
       </ListGroup>
     </>
-  )
+  );
 }
