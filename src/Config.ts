741f551c1d448fb5f7650d3c6973016197bd0c7a
aolopezeafit <aolopezh@eafit.edu.co>
2024-07-11 08:04:23 -0500

    Configurations management

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export const Config = {
-    VERSION: "1.24.06.18.13", 
+    VERSION: "1.24.07.11.07", 
     NODE_ENV: process.env.REACT_APP_NODE_ENV || 'development',
     HOST: process.env.REACT_APP_HOST || '127.0.0.1',
     PORT: process.env.REACT_APP_PORT || 3000,
