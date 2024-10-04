0f1db8c78c38ff3162a1aa8dbc13442543af0ae3
aolopezeafit <aolopezh@eafit.edu.co>
2024-10-03 19:15:28 -0500

    Element styles

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export const Config = {
-    VERSION: "1.24.09.26.20", 
+    VERSION: "1.24.10.03.19", 
     NODE_ENV: process.env.REACT_APP_NODE_ENV || 'development',
     HOST: process.env.REACT_APP_HOST || '127.0.0.1',
     PORT: process.env.REACT_APP_PORT || 3000,
